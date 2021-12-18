d553cf9c6150ba317fd3e410338438cc6572c2e0
lex <luohai2233@163.com>
2021-12-18 11:02:52 +0800

    update

--- requests_test.go
+++ requests_test.go
@@ -6,8 +6,10 @@ import (
 )
 
 func TestRequests(t *testing.T) {
-	req := NewRequest("https://api.mineor.xyz", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJVVUlEIjoiNzA3NDYwMTItMWRmZS00N2E1LWEzYjAtMGYxYzM0ZmY0ZDZlIiwiSUQiOjEsIlVzZXJuYW1lIjoiYWRtaW4iLCJOaWNrTmFtZSI6Iui2hee6p-euoeeQhuWRmCIsIkF1dGhvcml0eUlkIjoiMTAwIiwiQnVmZmVyVGltZSI6ODY0MDAsImV4cCI6MTYzOTUzMDMxOSwiaXNzIjoicW1QbHVzIiwibmJmIjoxNjM4OTI0NTE5fQ.U0Bhtn6-VduEEF0MIPcCZPbFpXKHHRXh7_G1uh63qXE")
-	req.SetProxy("99.0.85.1:808")
+
+	req := NewRequest("https://api.mineor.xyz", "")
+	//req.SetProxy("99.0.85.1:808")
+
 	code, err := req.Request("GET", "/project/listProjects", nil, nil)
 	fmt.Println(err)
 	fmt.Println(code)
